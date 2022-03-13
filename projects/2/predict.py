ddf0cedf541afcf9f2a8cea725eea025deb1cfa5
andrey_andreu <lutikovvvv@gmail.com>
2022-03-13 04:12:51 +0300

    -

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -23,7 +23,7 @@ model = load("2.joblib")
 #read and infere
 read_opts=dict(
         sep='\t', names=val_fields, index_col=False, header=None,
-        iterator=True, chunksize=100, na_values='\\N'
+        iterator=True, chunksize=100
 )
 
 for df in pd.read_csv(sys.stdin, **read_opts):
